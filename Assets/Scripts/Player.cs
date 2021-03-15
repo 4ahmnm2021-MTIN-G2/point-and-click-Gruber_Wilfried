5976a319dfddc4158af6f1440f07964c9ef930a4
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2021-03-15 10:38:12 +0100

    Aufgabe_02: attach character sprite to mouse in world space

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 mousePosScreen;
+    public Vector3 mousePosWorld;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosScreen = Input.mousePosition;
+        mousePosWorld = Camera.main.ScreenToWorldPoint(mousePosScreen);
+        this.transform.position = mousePosWorld;
+    }
+}
